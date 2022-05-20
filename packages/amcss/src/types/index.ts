2e2364de3ce49687d0f848bc19900a0b40efbd2f
alexzhang1030 <1642114555@qq.com>
2022-05-20 22:43:16 +0800

    style: lint code style

--- packages/amcss/src/types/index.ts
+++ packages/amcss/src/types/index.ts
@@ -6,12 +6,12 @@ export interface AmNode<T extends BasicObject, N extends BasicObject>
   /**
    * @description: cssStyleObject { width: '10px', ... }
    */
-  attrs: Record<string, string>;
+  attrs: Record<string, string>
 
   /**
    * @description: 允许插件在其配置中添加自己想要的配置
    */
-  options?: N;
+  options?: N
 }
 
 // 扫描器
@@ -19,30 +19,30 @@ export interface AmClass<T extends BasicObject> {
   /**
    * @description 完整的 class 名称
    */
-  className: string;
+  className: string
 
   /**
    * @description class 前缀 hover:w-10 -> hover
    */
-  prefix?: string;
+  prefix?: string
 
   /**
    * @description class 后缀 hover:bg-red/1 -> 1
    */
-  suffix?: string;
+  suffix?: string
 
   /**
    * @description: 基本 class 名称 hover:bg-red/1 -> bg-red
    */
-  pureName: string;
+  pureName: string
 
   /**
    * @description: 所用插件的唯一 id
    */
-  pid: string;
+  pid: string
 
   /**
    * @description: 允许插件在其配置中添加自己想要的配置
    */
-  extension?: T;
+  extension?: T
 }
