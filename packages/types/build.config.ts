dfa7e1d12633452ec426be2d041899ad53a27d5a
LoTwT <709937065@qq.com>
2022-05-24 19:26:26 +0800

    chore(types): update build config and export

--- packages/types/build.config.ts
+++ packages/types/build.config.ts
@@ -1,18 +1,10 @@
-import { resolve } from 'path'
 import { defineBuildConfig } from 'unbuild'
 
 export default defineBuildConfig({
-  entries: ['index'],
+  entries: ['src/index'],
   clean: true,
   declaration: true,
   rollup: {
-    emitCJS: true,
-    dts: {
-      respectExternal: false
-    }
-  },
-  alias: {
-    '@': resolve(__dirname, 'src'),
-    '~': resolve(__dirname)
+    emitCJS: true
   }
 })
